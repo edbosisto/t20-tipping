2841393cabdfd8f8aec62eaeab3cf055ad037baa
edbosisto <e.bosisto@gmail.com>
2022-10-12 13:18:37 +0200

    added models to admin

--- t20wc2022/tip/admin.py
+++ t20wc2022/tip/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Team, Venue, Match
 
 # Register your models here.
+
+admin.site.register(Team)
+admin.site.register(Venue)
+admin.site.register(Match)
