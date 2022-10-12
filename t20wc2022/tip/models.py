27cbf88c4fc089bfb026447a53d6ef4c7fea6fbd
edbosisto <e.bosisto@gmail.com>
2022-10-12 13:49:56 +0200

    minor to models

--- t20wc2022/tip/models.py
+++ t20wc2022/tip/models.py
@@ -6,11 +6,12 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(max_length=200)
-    matches_won = models.PositiveIntegerField()
+    ranking = models.PositiveIntegerField(default=None)
+    matches_won = models.PositiveIntegerField(default=0)
     eliminated = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.id + self.name
+        return f"{self.id}: {self.name}"
 
 
 class Venue(models.Model):
@@ -18,17 +19,16 @@ class Venue(models.Model):
     city = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.id + self.stadium
+        return f"{self.id}: {self.stadium}"
 
 
 class Match(models.Model):
     team1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="team1")
     team2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="team2")
     local_time = models.TimeField()
-    your_time = models.TimeField()
     date = models.DateField()
     venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
-    winner = models.ForeignKey(Team, default=None, on_delete=models.CASCADE, related_name="winner")
+    winner = models.ForeignKey(Team, blank=True, null=True, on_delete=models.CASCADE, related_name="winner")
 
     def __str__(self):
-        return self.id + ": " + self.team1 + " VS " + self.team2
+        return f"{self.id}: {self.team1} vs {self.team2}"
